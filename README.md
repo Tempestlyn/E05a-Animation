e48625df96fcaf594969b41c72fa978a24d97c06
jwringer <jwringer@iu.edu>
2019-09-25 20:30:53 -0400

    Completed Assignment, Updated Readme & license

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # E05a-Animation
 
+Within main1.py, you will find each line with a comment explaining the functionality of the line.
+
+Within main2.py, the game now runs using sprites from a game called "I wanna be the game". I resized the images, but for some reason, the tiled background wouldn't remove.
+
+
 We will begin to explore keyboard and mouse control in a Python Arcade program.
 
 When you are done, main1.py should contain the code on [http://arcade.academy/examples/move_mouse.html#move-mouse](http://arcade.academy/examples/move_mouse.html#move-mouse). Play with the code until you feel like you understand what is going on. Comment any uncommented line; feel free to ask for help!
--- main1.py
+++ main1.py
@@ -1,1 +1,83 @@
 #Copy the contents from http://arcade.academy/examples/move_mouse.html#move-mouse and see if you can figure out what is going on. Add comments to any uncommented lines
+
+import arcade
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+SCREEN_TITLE = "Move Mouse Example"
+
+
+class Ball:
+    def __init__(self, position_x, position_y, radius, color):
+
+        # Take the parameters of the init function above, and create instance variables out of them.
+        self.position_x = position_x
+        self.position_y = position_y
+        self.radius = radius
+        self.color = color
+
+    def draw(self):
+
+        #creates function to draw the ball
+        """ Draw the balls with the instance variables we have. """
+        arcade.draw_circle_filled(self.position_x, self.position_y, self.radius, self.color)
+
+
+class MyGame(arcade.Window):
+
+    def __init__(self, width, height, title):
+
+        # Call the parent class's init function
+        super().__init__(width, height, title)
+
+        # Make the mouse disappear when it is over the window.
+        # So we just see our object, not the pointer.
+        self.set_mouse_visible(False)
+
+        #sets background color
+        arcade.set_background_color(arcade.color.ASH_GREY)
+
+        # Creates our ball
+        self.ball = Ball(50, 50, 15, arcade.color.AUBURN)
+
+    def on_draw(self):
+        #renders the ball
+        """ Called whenever we need to draw the window. """
+        arcade.start_render()
+        self.ball.draw()
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        #sets location of ball to the mouse's x,y coords whenever the mouse is moved
+        """ Called to update our objects. Happens approximately 60 times per second."""
+        self.ball.position_x = x
+        self.ball.position_y = y
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+
+        #sets ball color to black on left click
+        print(f"You clicked button number: {button}")
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            self.ball.color = arcade.color.BLACK
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        """
+        Called when a user releases a mouse button.
+        """
+
+        #sets ball color to aubrun on release
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            self.ball.color = arcade.color.AUBURN
+
+
+def main():
+
+    #instantiates game window
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
